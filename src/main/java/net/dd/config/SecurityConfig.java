87c3bb2e001866c6dcb3ab233822af6766b7682d
Chika <1609879250@qq.com>
2021-04-16 17:58:40 +0800

    DD.Net By:Chika

--- src/main/java/net/dd/config/SecurityConfig.java
+++ src/main/java/net/dd/config/SecurityConfig.java
@@ -1,21 +1,36 @@
 package net.dd.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
-        http.formLogin().loginPage("login/login");
-        http.logout();
-        // 保存cookie两周
-        http.rememberMe();
-        // 关闭跨站检测
-        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/", "/..").permitAll()
+                .anyRequest().authenticated()
+        .and()
+                .formLogin()
+                .loginPage("/userRequest/login").permitAll()
+        .and()
+            .logout()
+        .and()
+            .rememberMe()
+        .and()
+            .csrf().disable();
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication().withUser("license");
+    }
+
 }
