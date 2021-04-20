503b4173188a8e2045ca206e20eb94bddf6af089
Chika <1609879250@qq.com>
2021-04-20 18:02:07 +0800

    DD.Net By:Chika

--- src/main/java/net/dd/controller/FileController.java
+++ src/main/java/net/dd/controller/FileController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
 import java.util.HashMap;
@@ -24,7 +25,7 @@ public class FileController {
 
     private DdDataService dataService;
     private QiNiuService qiNiuService;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final Map<Object, Object> JSON_DATA_MAP = new HashMap<>();
 
     @Autowired
@@ -36,7 +37,7 @@ public class FileController {
         this.qiNiuService = qiNiuService;
     }
 
-    @RequestMapping("/")
+    @RequestMapping("/upload.do")
     public String fileUpload(@RequestParam String filePath, @RequestParam String fileName, @RequestParam int fileType){
         String result;
         try {
@@ -45,7 +46,12 @@ public class FileController {
             logger.error(e.toString());
             return "";
         }
+        JSON_DATA_MAP.clear();
+        Map<Object, Object> map = new HashMap<>();
         dataService.insertFile(new DdData(fileType, fileName));
+        map.put("fileType", fileType);
+        map.put("fileName", fileName);
+        JSON_DATA_MAP.put(fileName, map);
         System.out.println("访问地址： " + result);
         return "";
     }
@@ -61,6 +67,12 @@ public class FileController {
         }
         System.out.println(result);
         return "";
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public Map<Object, Object> getJsonDataMap() {
+        return JSON_DATA_MAP;
     }
 
 }
